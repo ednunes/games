84c39224d5124979784fd9bf6a4c17b02997d95f
Eduardo Nunes <eduardonunes2525@gmail.com>
2016-10-12 00:01:50 -0300

    Add name of game project

---
+++ 2048/include/board.hpp
@@ -0,0 +1,40 @@
+#ifndef BOARD_H
+#define BOARD_H
+#define SQUARESNUMBERS 4
+
+
+#include <iomanip>
+#include <iostream>
+#include "square.hpp"
+#include <stdlib.h>
+#include <cstring>
+#include <algorithm>  
+#include <vector>
+
+class Square;
+
+class Board {
+	private:
+                Square board[SQUARESNUMBERS][SQUARESNUMBERS];
+
+	public:
+		Board();
+		~Board();
+				
+		void drawBoard();
+		bool generateNumberSquare(bool firstTime);
+		bool checkVictory(short value);
+		bool checkLost();
+
+
+		bool moveSquare(char move, bool statusGame);
+	
+		void setStatusSquareFalse();
+	
+		bool moveLeftOrUp(short defineM, short defineN, bool statusGame);
+		bool moveRightOrDown(short defineM, short defineN, bool statusGame);
+	
+		void testMatrix();	
+};
+
+#endif
